de70d43741e03f8fdd42feeed937e929552afd77
Marlon Nunes <marlon@arbeitstudio.com.br>
2024-08-12 19:51:47 -0300

    Adiciona visto por ultimo, sobre mim, e avatar no perfil e postagens dos usuarios

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ import sqlalchemy.orm as so
 from app import db
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -18,6 +19,11 @@ class User(UserMixin, db.Model):
 
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
 
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -26,6 +32,10 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return "<User {}>".format(self.username)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
 
 class Post(db.Model):
