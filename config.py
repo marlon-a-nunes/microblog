b935341964c202bbb76ea1f57a4eab1aad7463bd
Marlon Nunes <marlon@arbeitstudio.com.br>
2024-08-13 20:29:37 -0300

    Alterações cap. 7 tutorial https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-vii-error-handling

--- config.py
+++ config.py
@@ -12,3 +12,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
+
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = [
+        "marlon.nunes94@gmail.com"
+    ]  # Fazer um email de teste para desenvolvimento
